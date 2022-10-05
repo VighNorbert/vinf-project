745b7da2311fae06d8539b02fbaf81bc8ac30d09
Norbert Vígh <norbertusvigh@gmail.com>
2022-10-05 13:04:15 +0200

    init

---
+++ src/main/java/sk/vighnorbert/Main.java
@@ -0,0 +1,20 @@
+package sk.vighnorbert;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Hello world!");
+        String ln = reader.readLine();
+
+        System.out.println("Hello world!");
+        System.out.println(ln);
+        System.out.println("Hello world!");
+
+
+    }
+}
