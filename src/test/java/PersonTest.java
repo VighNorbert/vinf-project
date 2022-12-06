bc60232535e6ec835716ef89664409be3e3ec959
Norbert Vígh <norbertusvigh@gmail.com>
2022-12-06 16:16:33 +0100

    tests

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,13 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+}
